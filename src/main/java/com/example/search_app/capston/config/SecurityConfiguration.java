47c06a4df83db3b4d6d14ba4e4161727dc508fcc
bd-mahfuz <bt23.mahfuz@gmail.com>
2023-03-03 23:36:34 +0600

    spring security is added to the project

---
+++ src/main/java/com/example/search_app/capston/config/SecurityConfiguration.java
@@ -0,0 +1,62 @@
+package com.example.search_app.capston.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests((requests) -> requests
+                .anyRequest()
+                .authenticated());
+
+        http.formLogin()
+                .loginPage("/login")
+                .failureUrl("/loginError")
+                .permitAll()
+                .defaultSuccessUrl("/jobs", true);
+        http.logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .invalidateHttpSession(true)
+                .logoutSuccessUrl("/login");
+
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService);
+
+        http.authenticationManager(authenticationManagerBuilder.build());
+
+
+        return http.build();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(
+                "/admin_resources/**",
+                "/audio/**",
+                "/css/**",
+                "/fonts/**",
+                "/images/**",
+                "/js/**",
+                "/pdf/**",
+                "/webfonts/**",
+                "/webjars/**");
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
